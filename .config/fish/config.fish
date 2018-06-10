3f545d7201195a246a76085912e962ff58eb8971
Ryan Belgrave <rmb1993@gmail.com>
2018-06-10 11:50:31 -0500

    fix vscode trash issue, add gcloudsdk, add 2nd code ws and diff sopsdiffer

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,9 @@
 set -x GOPATH $HOME/IdeaProjects/go
-set -x PATH $PATH $GOPATH/bin
+set -x PATH $PATH $GOPATH/bin $HOME/bin
+
+# Fix vscode trash issue
+# https://github.com/Microsoft/vscode/issues/13189
+set -x ELECTRON_TRASH gio
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/rbelgrave/.google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/home/rbelgrave/.google-cloud-sdk/path.fish.inc'; else; . '/home/rbelgrave/.google-cloud-sdk/path.fish.inc'; end; end
