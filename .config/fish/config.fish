c1470b844e0c7f431d9d10c9ccf979eadab78468
Ryan Belgrave <ryan@belgrave.me>
2018-08-17 08:33:04 -0500

    disable VTE, disable mouse wheel in i3 bar, use snaps for everything

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 set -x GOPATH $HOME/IdeaProjects/go
-set -x PATH $PATH $GOPATH/bin $HOME/bin
+set -x PATH $PATH $GOPATH/bin $HOME/bin /snap/bin
 
 # Fix vscode trash issue
 # https://github.com/Microsoft/vscode/issues/13189
@@ -7,3 +7,6 @@ set -x ELECTRON_TRASH gio
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/rbelgrave/.google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/home/rbelgrave/.google-cloud-sdk/path.fish.inc'; else; . '/home/rbelgrave/.google-cloud-sdk/path.fish.inc'; end; end
+
+# Disable VTE because I like being in the home directory
+set -x VTE_VERSION 0
